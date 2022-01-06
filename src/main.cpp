adf967986fc980ef43d08eef780a5edfb1e12374
Bartolomej Orloj <bart.orloj@gmail.com>
2022-01-06 18:01:32 +0100

    example wifi settings for platformio

--- src/main.cpp
+++ src/main.cpp
@@ -3,66 +3,73 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-const char* ssid = "nwifi";
-const char* password = "kilT524Cent";
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
 
 void setup() {
-	Serial.begin(115200);
-	Serial.println("Booting");
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(ssid, password);
-	while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-		Serial.println("Connection Failed! Rebooting...");
-		delay(5000);
-		ESP.restart();
-	}
+    Serial.begin(115200);
+    Serial.println("Booting");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("Connection Failed! Rebooting...");
+        delay(5000);
+        ESP.restart();
+    }
 
-	// Port defaults to 3232
-	// ArduinoOTA.setPort(3232);
+    // Port defaults to 3232
+    // ArduinoOTA.setPort(3232);
 
-	// Hostname defaults to esp3232-[MAC]
-	// ArduinoOTA.setHostname("myesp32");
+    // Hostname defaults to esp3232-[MAC]
+    // ArduinoOTA.setHostname("myesp32");
 
-	// No authentication by default
-	// ArduinoOTA.setPassword("admin");
+    // No authentication by default
+    // ArduinoOTA.setPassword("admin");
 
-	// Password can be set with it's md5 value as well
-	// MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-	// ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+    // Password can be set with it's md5 value as well
+    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-	ArduinoOTA
-		.onStart([]() {
-				String type;
-				if (ArduinoOTA.getCommand() == U_FLASH)
-				type = "sketch";
-				else // U_SPIFFS
-				type = "filesystem";
+    ArduinoOTA
+        .onStart([]() {
+                String type;
+                if (ArduinoOTA.getCommand() == U_FLASH)
+                type = "sketch";
+                else // U_SPIFFS
+                type = "filesystem";
 
-				// NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-				Serial.println("Start updating " + type);
-				})
-	.onEnd([]() {
-			Serial.println("\nEnd");
-			})
-	.onProgress([](unsigned int progress, unsigned int total) {
-			Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-			})
-	.onError([](ota_error_t error) {
-			Serial.printf("Error[%u]: ", error);
-			if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-			else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-			else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-			else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-			else if (error == OTA_END_ERROR) Serial.println("End Failed");
-			});
+                // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+                Serial.println("Start updating " + type);
+                })
+    .onEnd([]() {
+            Serial.println("\nEnd");
+            })
+    .onProgress([](unsigned int progress, unsigned int total) {
+            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+            })
+    .onError([](ota_error_t error) {
+            Serial.printf("Error[%u]: ", error);
+            if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+            else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+            else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+            else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+            else if (error == OTA_END_ERROR) Serial.println("End Failed");
+            });
 
-	ArduinoOTA.begin();
+    ArduinoOTA.begin();
 
-	Serial.println("Ready");
-	Serial.print("IP address: ");
-	Serial.println(WiFi.localIP());
+    Serial.println("Ready");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
-
+double last;
 void loop() {
-	ArduinoOTA.handle();
+    ArduinoOTA.handle();
+
+    if (millis()-last>1000){
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
+        last=millis();
+    }
 }
+
